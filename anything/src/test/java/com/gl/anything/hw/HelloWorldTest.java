c304b7b4f46d915ac55c3c481cee4311d0030487
gavin2lee <gavin2lee@163.com>
2016-09-20 16:43:29 +0800

    checkstyle

---
+++ anything/src/test/java/com/gl/anything/hw/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package com.gl.anything.hw;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void testSay() {
+//		fail("Not yet implemented");
+		int a = 2;
+		assertTrue(2==a);
+	}
+
+}
